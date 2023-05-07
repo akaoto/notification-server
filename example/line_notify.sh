229fc4b8d9471828ecba0ffa2aaecf815163030d
akaoto <redmusic@outlook.com>
2023-05-08 01:32:01 +0900

    bashスクリプトを追加

---
+++ example/line_notify.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+MESSAGE=$1
+ACCESS_TOKEN=`curl -Ls ${NOTIFICATION_SERVER_URL} -d "{\"api_token\": \"${NOTIFICATION_SERVER_API_TOKEN}\"}" | jq .access_token`
+curl -Ls -o /dev/null ${NOTIFICATION_SERVER_URL}/line/notify -H "Authorization: Bearer ${ACCESS_TOKEN}" -d "{\"message\": \"${MESSAGE}\"}"
